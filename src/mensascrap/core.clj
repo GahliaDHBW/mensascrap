4cc27d5d736e6a445fa38027fe64adf583896092
port19 <port19@port19.xyz>
2022-11-17 10:35:34 +0100

    parse one meal

--- src/mensascrap/core.clj
+++ src/mensascrap/core.clj
@@ -4,6 +4,37 @@
   (:require [hickory.core :refer :all])
 )
 
-(def raw (slurp "sample.html"))
-(as-hickory (parse raw))
-(as-hiccup (parse raw))
+(def raw (->> "sample.html"
+              (#(slurp %))
+              (#(parse %))
+              (#(as-hiccup %))
+))
+(def bone (->> raw
+               (#(second %))
+               (#(get % 3))
+               (#(get % 5))
+               (#(get % 2))
+               (#(get % 3))
+               (#(get % 3))
+))
+(def whale (->> bone
+                (#(get % 3)) ;;meal: 3 -> Wahlessen 1, 5 -> Wahlessen 2, 7 -> Wahlessen 3
+                (#(get % 2)) ;;day: 2-6 maps to mon-fri
+                (#(get % 2))
+))
+(def meals [3 5 7])
+(def days [2 3 4 5 6])
+
+(defn fishknife
+  [largefish]
+  (def a (->> largefish
+       (#(get % 2))
+       (#(get % 2))
+       (#(get % 2))))
+  (def b (->> largefish
+       (#(get % 3))
+       (#(get % 2))
+       (#(get % 2))))
+  [a b]
+)
+(fishknife whale)
